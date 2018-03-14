138222641cc09c79df8c64ce3ba04fb77cee5281
Paulo <paulofore@gmail.com>
2018-03-14 20:46:34 -0300

    Forms

--- src/pages/reserva/reserva.ts
+++ src/pages/reserva/reserva.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
-
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 /**
  * Generated class for the ReservaPage page.
  *
@@ -15,8 +15,15 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class ReservaPage {
 
+  reservaForm:  FormGroup;
+
   constructor(public navCtrl: NavController, public navParams: NavParams, 
-            public viewCtrl:ViewController ) {
+            public viewCtrl:ViewController, private formBuilder:FormBuilder ) {
+        this.reservaForm = this.formBuilder.group({
+          guests: 3,
+          smoking: false,
+          dateTime: ['',Validators.required]
+        });
   }
 
   ionViewDidLoad() {
@@ -27,4 +34,8 @@ export class ReservaPage {
     this.viewCtrl.dismiss();
   }
 
+  onSubmit() {
+    console.log(this.reservaForm.value);
+    this.viewCtrl.dismiss();
+  }
 }
