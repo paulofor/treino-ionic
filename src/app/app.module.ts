5fb1bec05cbc43a0ced58aa575d6a5986421c9d2
Paulo <paulofore@gmail.com>
2018-03-22 20:57:34 -0300

    Notificacoes - sem testar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { LoginPage } from '../pages/login/login';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { LocalNotifications } from '@ionic-native/local-notifications';
 import { DishProvider } from '../providers/dish/dish';
 import { LeaderProvider } from '../providers/leader/leader';
 import { PromocaoProvider } from '../providers/promocao/promocao';
